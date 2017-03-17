21c36fea8f2d8d37681d3d661892a1b4d5a46070
Mike <mikelswartz@gmail.com>
2017-03-17 14:23:23 -0400

    Tested and corrected provision file

--- nodeJS.provision.sh
+++ nodeJS.provision.sh
@@ -3,6 +3,8 @@ cd /vagrant/
 yum update
 yum install -y vim git tree wget curl unzip screen
 yum -y groupinstall "Development Tools"
+
+:<< 'END'
 cd /usr/src
 if [ -f "/vagrant/node-v0.10.4.tar.gz" ]
 then
@@ -10,12 +12,14 @@ then
 else
   wget http://nodejs.org/dist/v0.10.4/node-v0.10.4.tar.gz
 fi
-
 tar zxf node-v0.10.4.tar.gz
 cd node-v0.10.4
 ./configure
 make 
 make install 
+END
+
+yum install npm -y
 #PATH
 echo "export PATH=$PATH:/usr/local/git/bin:/usr/local/bin" >> /etc/bashrc
 npm -g install express express-generator supervisor
